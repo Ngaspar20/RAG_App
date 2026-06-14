56e9c08f7890959f5a6974342ff770d0b5d8aa62
Nuno <64129296+Ngaspar20@users.noreply.github.com>
2026-06-14 10:07:37 +0200

    Fix import path for RecursiveCharacterTextSplitter

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import tempfile
 import uuid
 
 from groq import Groq
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
